62a42735dc3e4020b24717f2891f351b7a3d2f7c
Masahiro Fujioka <fujioka8700@gmail.com>
2024-03-25 11:50:01 +0900

    [update] sinカーブ。

--- source.cpp
+++ source.cpp
@@ -1,3 +1,5 @@
+#define _USE_MATH_DEFINES
+#include <math.h>
 #include <Dxlib.h>
 
 int WINAPI WinMain(
@@ -14,29 +16,14 @@ int WINAPI WinMain(
 
 	if (DxLib_Init() == -1) return -1;
 
-	DrawLine(0, 0, 640, 480, GetColor(255, 0, 0), 3);
-	DrawLineAA(640, 0, 0, 480, GetColor(255, 0, 0), 3);
+	DrawLine(320, 110, 320, 370, GetColor(128, 128, 128));
+	DrawLine(30, 240, 610, 240, GetColor(128, 128, 128));
 
-	DrawLine(220, 0, 220, 480, GetColor(0, 255, 0), 3);
-	DrawLineAA(420, 0, 420, 480, GetColor(0, 255, 0), 3);
-
-	DrawLine(0, 160, 640, 160, GetColor(0, 0, 255), 3);
-	DrawLineAA(0, 320, 640, 320, GetColor(0, 0, 255), 3);
-												  
-	WaitKey();
-	ClearDrawScreen();
-
-	DrawBox(55, 90, 155, 190, GetColor(255, 255, 0), TRUE);
-	DrawBoxAA(55, 280, 155, 380, GetColor(255, 255, 0), TRUE);
-
-	DrawCircle(255, 140, 50, GetColor(255, 0, 255), TRUE);
-	DrawCircleAA(255, 330, 50, 32, GetColor(255, 0, 255), TRUE);
-
-	DrawOval(405, 140, 50, 30, GetColor(0, 255, 255), TRUE);
-	DrawOvalAA(405, 330, 50, 30, 32, GetColor(0, 255, 255), TRUE);
-
-	DrawTriangle(555, 90, 505, 190, 605, 190, GetColor(255, 255, 255), TRUE);
-	DrawTriangleAA(555, 280, 505, 380, 605, 380, GetColor(255, 255, 255), TRUE);
+	for (int x = -290; x < 290; x++)
+	{
+		int y = (int)(sin(M_PI * x / 180.0f) * 100);
+		DrawPixel(320 + x, 240 - y, GetColor(255, 0, 0));
+	}
 
 	WaitKey();
 
