f4c4f676f29cfbeae34928a5ed9a5f54cec7cde8
Masahiro Fujioka <fujioka8700@gmail.com>
2024-04-01 14:32:51 +0900

    [update] 多角形の回転と変化

--- source.cpp
+++ source.cpp
@@ -59,15 +59,22 @@ int WINAPI WinMain(
 
 	if (DxLib_Init() == -1) return -1;
 
-	const float RADIUS = 50, SPEED_ANGLE = 360.0f / 2.0f;
+	const float RADIUS = 80, SPEED_ANGLE = 360.0f / 2.0f, INTERVAL = 1.5f;
 	const float x = 320, y = 240;
+	const int   POLYGON_MIN = 3, POLYGON_MAX = 10;
 	float    angle, speedAngle;
 	LONGLONG fpsTimer, deltaTimer;
+	int      n, dn;
+	float    intervalTime;
 
 	fpsTimer = deltaTimer = GetNowHiPerformanceCount();
 
 	angle = 0;
 	speedAngle = SPEED_ANGLE;
+
+	n = POLYGON_MIN;
+	dn = 1;
+	intervalTime = INTERVAL;
 
 	SetDrawScreen(DX_SCREEN_BACK);
 
@@ -81,10 +88,20 @@ int WINAPI WinMain(
 			angle -= 360.0f;
 		}
 
-		ClearDrawScreen();
-		
+		intervalTime -= deltaTime;
+		if (intervalTime <= 0.0f)
+		{
+			intervalTime = INTERVAL;
 
-		const int n = 10;
+			n += dn;
+			if (n >= POLYGON_MAX || n <= POLYGON_MIN)
+			{
+				dn *= -1;
+			}
+		}
+		
+		ClearDrawScreen();
+
 		for (int i = 0; i < n; i++)
 		{
 			float dx = (float)(RADIUS * cos(M_PI * (angle + i * 360.0 / n) / 180.0));
@@ -92,7 +109,7 @@ int WINAPI WinMain(
 			float ex = (float)(RADIUS * cos(M_PI * (angle + (i + 1) * 360.0 / n) / 180.0));
 			float ey = (float)(RADIUS * sin(M_PI * (angle + (i + 1) * 360.0 / n) / 180.0));
 			int color = 255 - i * (255 - 64) / n;
-			DrawTriangleAA(x, y, x + dx, y + dy, x + ex, y + ey, GetColor(color, color, 0), TRUE);
+			DrawTriangleAA(x - 40, y - 40, x + dx, y + dy, x + ex, y + ey, GetColor(color, color, 0), TRUE);
 		}
 
 		FpsDraw(&fpsTimer);
